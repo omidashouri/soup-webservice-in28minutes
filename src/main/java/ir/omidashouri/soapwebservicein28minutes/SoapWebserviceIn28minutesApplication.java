a0f33e8f205708d2d6988f88646b5a821641ce96
Omid Ashouri <omidashouri@gmail.com>
2019-08-12 11:36:40 -0700

    first commit

---
+++ src/main/java/ir/omidashouri/soapwebservicein28minutes/SoapWebserviceIn28minutesApplication.java
@@ -0,0 +1,13 @@
+package ir.omidashouri.soapwebservicein28minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebserviceIn28minutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapWebserviceIn28minutesApplication.class, args);
+    }
+
+}
