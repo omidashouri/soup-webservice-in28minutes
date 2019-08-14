713d9c56f2cb28249015a3e4a9307ecca5b4645b
Omid Ashouri <omidashouri@gmail.com>
2019-08-14 12:21:24 -0700

    up to 12 : omidashouri : 98.05.23 11:50

---
+++ src/main/java/ir/omidashouri/soap/CourseDetailsEndpoint.java
@@ -0,0 +1,27 @@
+package ir.omidashouri.soap;
+
+import ir.omidashouri.courses.CourseDetails;
+import ir.omidashouri.courses.GetCourseDetailsRequest;
+import ir.omidashouri.courses.GetCourseDetailsResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CourseDetailsEndpoint {
+
+    @PayloadRoot(namespace = "http://omidashouri.ir/courses", localPart = "GetCourseDetailsRequest")
+    @ResponsePayload
+    public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request){
+
+        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+
+        CourseDetails courseDetails = new CourseDetails();
+        courseDetails.setId(request.getId());
+        courseDetails.setName("Microservices Course");
+        courseDetails.setDescription("that would be a wonderful course");
+
+        return response;
+    }
+}
